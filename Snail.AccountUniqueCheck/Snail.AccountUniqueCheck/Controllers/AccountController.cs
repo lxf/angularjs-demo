5f93c00ccec84e827cb38364052cdc8174ebc93d
unknown <snail.li@hdt-p266.hdtworld.com>
2015-04-02 13:28:09 +0800

    增加了一个angularjs与MVC后台唯一性验证的demo

---
+++ Snail.AccountUniqueCheck/Snail.AccountUniqueCheck/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace Snail.AccountUniqueCheck.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult AccountCheck()
+        {
+            string value = Request["value"];
+            string property = Request["property"];
+            if (value == "www.upsnail.com")
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
